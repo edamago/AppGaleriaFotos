a625ed3c601b779b16398c42c86bdd28b514a27a
edamago <daniel.martel@outlook.com>
2024-03-06 22:48:43 -0500

    quinto commit

--- app/src/main/java/pe/edu/idat/appgaleriafotos/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/appgaleriafotos/MainActivity.kt
@@ -2,11 +2,15 @@ package pe.edu.idat.appgaleriafotos
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import pe.edu.idat.appgaleriafotos.databinding.ActivityMainBinding
 import java.io.File
 
@@ -37,6 +41,12 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         val intentCamara = Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
             it.resolveActivity(packageManager).also {
                 componente ->
+                crearImagenFoto()
+                val fotoUri:Uri = FileProvider.getUriForFile(
+                    applicationContext,
+                    "pe.edu.idat.appgaleriafotos.fileprovider",
+                    archivo)
+                it.putExtra(MediaStore.EXTRA_OUTPUT,fotoUri)
             }
         }
     }
@@ -47,8 +57,23 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         resultado ->
         if(resultado.resultCode== RESULT_OK){
             val foto=resultado.data!!
-            val fotoBitmap = foto.extras!!.get("data") as Bitmap
+            val fotoBitmap = convertirFotoBitmap()
             binding.ivFoto.setImageBitmap(fotoBitmap)
         }
+    }
+
+    private fun convertirFotoBitmap():Bitmap{
+        return BitmapFactory.decodeFile(archivo.toString())
+    }
+
+    private fun crearImagenFoto(){
+        val directorio = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+
+        archivo  = File.createTempFile(
+            "IMG_${System.currentTimeMillis()}",
+            "jpg",
+            directorio
+        )
+        rutaFotoActual=archivo.absolutePath
     }
 }
